c1406c32dae8e3098b6b5a26551e7d52b49e46fa
vmuser <vmuser@192.168.2.132>
2018-03-21 15:36:04 +0100

    Einfaches Gui zum Ausbauen hinzugefügt

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -1,10 +1,33 @@
 package Gui;
+import javax.swing.*;        
 
 public class Gui {
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
 
-	}
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
 
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
