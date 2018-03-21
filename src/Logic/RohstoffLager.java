a1041326f0fca0dd65a5a259018a82d26996385c
vmuser <vmuser@192.168.2.130>
2018-03-21 16:08:45 +0100

    SpecialFUnctions methoden erstellt, Rohstofflager Add Methoden hinzugefügt ~Fabian Zeller

--- src/Logic/RohstoffLager.java
+++ src/Logic/RohstoffLager.java
@@ -1,5 +1,21 @@
 package Logic;
 
 public class RohstoffLager {
+private double holzmenge;
+private double goldmenge;
+private double steinmenge;
+
+public void addHolzmenge(double HolzRein){
+	this.holzmenge = this.holzmenge + HolzRein;
+}
+
+public void addSteinmenge(double SteinRein){
+	this.steinmenge = this.steinmenge + SteinRein;
+}
+
+public void addGoldmenge(double GoldRein){
+	this.goldmenge = this.goldmenge + GoldRein;
+}
+
 
 }
--- src/Logic/SpecialFunctions.java
+++ src/Logic/SpecialFunctions.java
@@ -1,5 +1,15 @@
 package Logic;
 
 public class SpecialFunctions {
+	
+public boolean HackerAngriff(){
+	
+	return false;
+	
+}
 
+public boolean UltraUpgrade(){
+	return false;
+	
+}
 }
