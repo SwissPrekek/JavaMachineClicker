12448d3c02a2feb1b0e8bf574091c8f8f3c46129
vmuser <vmuser@192.168.2.132>
2018-03-21 16:04:10 +0100

    Fenstergrösse hinzugefügt

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -4,15 +4,16 @@ import javax.swing.*;
 public class Gui {
     public Gui() {
         //Fenster erstellen
-        JFrame frame = new JFrame("HelloWorldSwing");
+        JFrame frame = new JFrame("JavaMachineClicker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Fügt Label hinzu
-        JLabel label = new JLabel("Testgui");
+        JLabel label = new JLabel("JavaMachineClicker");
         frame.getContentPane().add(label);
 
         //Zeigt das Fenster an
         frame.pack();
         frame.setVisible(true);
+        frame.setSize(1000, 500);
     }
 }
