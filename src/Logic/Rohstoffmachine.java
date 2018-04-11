a6ea747caccf79bfbfe29495371e33be8596f157
vmuser <vmuser@192.168.2.132>
2018-04-11 13:02:07 +0200

    Bugfix - -Rudin

--- src/Logic/Rohstoffmachine.java
+++ src/Logic/Rohstoffmachine.java
@@ -14,5 +14,23 @@ public class Rohstoffmachine implements RzFace{
 		
 		
 	}
+
+	@Override
+	public boolean isRunning() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setRunning(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Runnable newObj() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
